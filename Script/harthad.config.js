0fdd1c17a06c44ae1cd3b108500cacb753aa9ee5
ASISBusines's <74536670+ASISBusiness@users.noreply.github.com>
2025-05-28 10:51:33 -0400

    Create harthad.config.js
    require("@nomicfoundation/hardhat-toolbox");
    require("dotenv").config();
    
    module.exports = {
      solidity: "0.8.19",
      networks: {
        polygon: {
          url: "https://polygon-rpc.com",
          accounts: [process.env.PRIVATE_KEY]
        }
      }
    };

---
+++ Script/harthad.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    polygon: {
+      url: "https://polygon-rpc.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
